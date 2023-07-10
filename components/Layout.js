93c6a258f3edd95fa89c6cb1759884c54b8bd0ad
Alex <43852391+AlexTechNoir@users.noreply.github.com>
2023-07-10 19:52:38 +0300

    3

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
   return (
     <>
       <div style={{ padding: '1em', fontFamily: 'Roboto, sans-serif', width: '1000px', lineHeight: 1.5 }}>
-        This is a reproduction example for Jotai's useHydrationAtoms. This app has global Layout component, that has useEffect. With empty arr of deps, useEffect should launch only on Layout's mount, but because of useHydrationAtoms, Layout remounts on every route change, causing useEffect to relaunch as well. To see it, open console and click on links
+        This is a reproduction example for Jotai's useHydrateAtoms. This app has global Layout component, that has useEffect. With empty arr of deps, useEffect should launch only on Layout's mount, but because of useHydrateAtoms, Layout remounts on every route change, causing useEffect to relaunch as well. To see it, open console and click on links
       </div>
       {children}
     </>
